6708098ad3c9244ce1ff4b1bf9fbfd537966ed42
ThorTheTroll <hasannawadd@gmail.com>
2024-02-14 22:05:59 +0200

    first navbar version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,114 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
+import './script.js'
+import './script2.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <div class='cursor' id="cursor"></div>
+      <div class='cursor2' id="cursor2"></div>
+      <div class='cursor3' id="cursor3"></div>
+      <nav class="navbar navbar-expand-custom navbar-mainbg">
+        <div class="nav-but-wrap">
+          <div class="menu-icon hover-target">
+            <span class="menu-icon__line menu-icon__line-left"></span>
+            <span class="menu-icon__line"></span>
+            <span class="menu-icon__line menu-icon__line-right"></span>
+          </div>
+          <div class="nav">
+            <div class="nav__content">
+              <ul class="nav__list">
+                <li class="nav__list-item active-nav"><a href="#" class="hover-target">Home</a></li>
+                <li class="nav__list-item"><a href="#" class="hover-target">Portfolio</a></li>
+                <li class="nav__list-item"><a href="#" class="hover-target">Studio</a></li>
+                <li class="nav__list-item"><a href="#" class="hover-target">News</a></li>
+                <li class="nav__list-item"><a href="#" class="hover-target">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <img class="mylogo" src="/assets/Progroup-lb-logo-1x.png" />
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul class="navbar-nav ml-auto">
+            <div class="hori-selector"><div class="left"></div><div class="right"></div></div>
+            <li class="nav-item active">
+              <a class="nav-link" href="javascript:void(0);"><i class="fas fa-tachometer-alt"></i>Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="javascript:void(0);"><i class="far fa-address-book"></i>About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="javascript:void(0);"><i class="far fa-clone"></i>Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="javascript:void(0);"><i class="far fa-calendar-alt"></i>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div id="switch" class="hover-target switched" style={{
+          "margin-left":"50%"
+        }}>
+				<div id="circle"
+          ></div>
+			</div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+
+
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+      <div>
+        <p>TEST</p>
+      </div>
+
+
+      <div>
+        <p>TEST</p>
+      </div>
     </div>
   );
 }
---
+++ src/script2.js
@@ -0,0 +1,111 @@
+import jQuery from 'jquery';
+/* global $ */
+var script = document.createElement('script')
+script.src = "https://code.jquery.com/jquery-3.6.0.min.js"
+document.head.appendChild(script)
+script.onload = function () {
+    $(document).ready(function () {
+        "use strict";
+
+        // Page cursors
+        var t = document.getElementById("cursor"),
+            e = document.getElementById("cursor2"),
+            i = document.getElementById("cursor3");
+
+        // Ensure that the variables t, e, and i are defined before using them
+        if (t && e && i) {
+            document.getElementsByTagName("body")[0].addEventListener("mousemove", function (n) {
+                t.style.left = n.clientX + "px";
+                t.style.top = n.clientY + "px";
+                e.style.left = n.clientX + "px";
+                e.style.top = n.clientY + "px";
+                i.style.left = n.clientX + "px";
+                i.style.top = n.clientY + "px";
+            });
+        }
+
+        // Ensure that the variables t, e, and i are defined before using them
+        if (t && e && i) {
+            function n(t) {
+                e.classList.add("hover");
+                i.classList.add("hover");
+            }
+
+            function s(t) {
+                e.classList.remove("hover");
+                i.classList.remove("hover");
+            }
+
+            s();
+
+            var hoverTargets = document.querySelectorAll(".hover-target");
+
+            // Ensure that the hover targets are defined before iterating over them
+            if (hoverTargets) {
+                for (var a = hoverTargets.length - 1; a >= 0; a--) {
+                    o(hoverTargets[a]);
+                }
+            }
+
+            function o(t) {
+                t.addEventListener("mouseover", n);
+                t.addEventListener("mouseout", s);
+            }
+        }
+
+        // Navigation
+
+        var app = function () {
+            var body = undefined;
+            var menu = undefined;
+            var menuItems = undefined;
+            var horiSelectorDiv = undefined;
+            
+            var init = function init() {
+                body = document.querySelector('body');
+                menu = document.querySelector('.menu-icon');
+                horiSelectorDiv = document.querySelector('.hori-selector');
+                menuItems = document.querySelectorAll('.nav__list-item');
+                body.classList.add("light");
+                // horiSelectorDiv.classList.add("light");
+                applyListeners();
+            };
+
+            var applyListeners = function applyListeners() {
+                // Ensure that the menu is defined before adding the event listener
+                if (menu) {
+                    menu.addEventListener('click', function () {
+                        return toggleClass(body, 'nav-active');
+                    });
+                }
+            };
+
+            var toggleClass = function toggleClass(element, stringClass) {
+                if (element.classList.contains(stringClass)) element.classList.remove(stringClass); else element.classList.add(stringClass);
+            };
+
+            init();
+        }();
+
+        // Switch light/dark
+
+        $("#switch").on('click', function () {
+            if ($("body").hasClass("light")) {
+                $("body").removeClass("light");
+                $("#switch").removeClass("switched");
+            } else {
+                $("body").addClass("light");
+                $("#switch").addClass("switched");
+            }
+        });
+
+        $(".nav__list-item").on('click', function () {
+            var stringClass = 'active-nav';
+            var old_active = document.querySelector('.active-nav');
+            old_active.classList.remove(stringClass);
+            var new_active = $(this);
+            new_active.addClass(stringClass);
+        });
+
+    });
+};
